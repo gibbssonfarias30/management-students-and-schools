444b818d16ae567d2ec0eb5d72bfc5db7c5e428b
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-03 11:24:18 -0500

    enabled eureka discovery client and feign clients

--- school-service/src/main/java/com/backfcdev/schoolservice/SchoolServiceApplication.java
+++ school-service/src/main/java/com/backfcdev/schoolservice/SchoolServiceApplication.java
@@ -2,8 +2,12 @@ package com.backfcdev.schoolservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SchoolServiceApplication {
 
 	public static void main(String[] args) {
